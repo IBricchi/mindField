727514d7edb849d46caec365fbad6da1c143e8ba
IBricchi <ignacio.bricchi@gmail.com>
2017-01-24 17:13:30 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mind Field</title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script type="js/main.js"></script>
+</head>
+<body>
+
+	
+
+</body>
+</html>
